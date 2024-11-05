6b2787ce95b67d258c038c3f5d11117f4db3e7c7
Walisson oliveira <walissonoliveira1@outlook.com>
2024-11-05 11:38:17 -0300

    inserindo valores no banco

--- conectormysql.py
+++ conectormysql.py
@@ -6,4 +6,25 @@ banco = mysql.connector.connect(
     password="root",
     database= "bd_walisson"
 )
-print(banco)
+
+meucursor = banco.cursor()
+pesquisa = 'select * from alunos;'
+meucursor.execute(pesquisa)
+
+resultado = meucursor.fetchall()
+for x in resultado:
+    print(x)
+
+
+nome1  = input("Digite seu nome")
+telefone1 = input("Digite ")
+
+sql = "INSERT INTO alunos(nome, telefone) VALUES (%s,%s)"
+data = (nome1, telefone1)
+meucursor.execute(sql, data)
+banco.commit()
+
+meucursor.close()
+banco.close()
+
+
